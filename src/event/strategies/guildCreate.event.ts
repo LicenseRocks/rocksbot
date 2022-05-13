78f596eb5c7263c2fc6730883adfbf3dbba3298c
Igor Klepacki <neg4n@icloud.com>
2022-05-13 09:19:26 +0200

    fix(bot): welcome message error handling

--- src/event/strategies/guildCreate.event.ts
+++ src/event/strategies/guildCreate.event.ts
@@ -4,6 +4,8 @@ import { MessageFormat } from "@infrastructure/helpers/message-format";
 import { EventStrategy } from "@event/event.strategy";
 import { welcomeMessages } from "@infrastructure/translations";
 import { Injectable } from "@infrastructure/dependency-injection/injectable";
+import { retrieveFaulty } from "@infrastructure/helpers/faulty-pair";
+import { isNil } from "lodash";
 
 @Injectable()
 export class GuildCreateEventStrategy implements EventStrategy {
@@ -15,17 +17,28 @@ export class GuildCreateEventStrategy implements EventStrategy {
     const guildOwner = await guild.fetchOwner();
     const guildOwnerDirectChannel = await guildOwner.createDM();
 
-    await guildOwnerDirectChannel.send({
-      embeds: [
-        new MessageEmbed()
-          .setColor(MessageFormat.color.neutralGray)
-          .setTitle(welcomeMessages.en.title)
-          .setAuthor({ iconURL: this.client.user.avatarURL(), name: welcomeMessages.en.authorName })
-          .setDescription(welcomeMessages.en.description)
-          .addFields(welcomeMessages.en.fields)
-          .setFooter({ text: welcomeMessages.en.footer }),
-        // TODO: Deutsch translations embed
-      ],
-    });
+    const [_, error] = await retrieveFaulty(
+      guildOwnerDirectChannel.send({
+        embeds: [
+          new MessageEmbed()
+            .setColor(MessageFormat.color.neutralGray)
+            .setTitle(welcomeMessages.en.title)
+            .setAuthor({
+              iconURL: this.client.user.avatarURL(),
+              name: welcomeMessages.en.authorName,
+            })
+            .setDescription(welcomeMessages.en.description)
+            .addFields(welcomeMessages.en.fields)
+            .setFooter({ text: welcomeMessages.en.footer }),
+          // TODO: Deutsch translations embed
+        ],
+      })
+    );
+
+    if (!isNil(error)) {
+      console.log(
+        `Could not send welcome message to ${guildOwner.user.tag} (${guildOwner.user.id}).`
+      );
+    }
   }
 }
